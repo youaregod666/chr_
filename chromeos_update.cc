c67020f1914d9988a3105e6cdf5ad0164c149fac
Andrew de los Reyes <adlr@chromium.org>
2010-02-22 11:05:33 -0800

    cros: support for calling AU
    Review URL: http://codereview.chromium.org/600120

--- chromeos_update.cc
+++ chromeos_update.cc
@@ -9,6 +9,12 @@
 #include <unistd.h>
 
 #include <cstdlib>
+#include <string>
+#include <base/scoped_ptr.h>
+
+#include <glib.h>
+
+using std::string;
 
 namespace chromeos {
 
@@ -19,64 +25,128 @@ void Destruct(const UpdateInformation& x) {
   delete x.version_;
 }
 
+// Searches for a line "key=some value" for a given key 'key'
+// and returns the value part of the line
+string ValueForKey(const char* const haystack, const string& key) {
+  string use_key = key + "=";
+
+  const char* needle = NULL;
+
+  // See if haystack starts with key
+  if (!strncmp(haystack, use_key.data(), use_key.size())) {
+    needle = haystack;
+  } else {
+    use_key = string("\n") + use_key;
+    needle = strstr(haystack, use_key.c_str());
+  }
+  if (!needle)
+    return "";
+  needle += use_key.size();
+  const char* end = needle + 1;
+  while (*end != '\0' && *end != '\n') {
+    end++;
+  }
+  return string(needle, end - needle);
+}
+
 }  // namespace
 
 extern "C"
 bool ChromeOSUpdate(UpdateInformation* information) {
-  // This code mochs in the UpdateInformation. To be filled in by Andrew.
+  scoped_array<char*> envp(new char*[2]);
+  envp[0] = strdup("PATH=/bin:/sbin:/usr/bin:/usr/sbin");
+  envp[1] = NULL;
+  
+  scoped_array<char*> argv(new char*[3]);
+  argv[0] = strdup("/opt/google/memento_updater/suid_exec");
+  argv[1] = strdup("/opt/google/memento_updater/memento_updater.sh");
+  argv[2] = NULL;
 
-  // simulate time to check.
-  ::sleep(static_cast<unsigned>(std::rand()) % 30);
+  gint exit_status = 0;
+  gchar* child_stdout = NULL;
+  GError* err = NULL;
 
-  information->destruct_ = &Destruct;
+  gboolean success = g_spawn_sync(NULL,  // Use parent's working dir
+                                  argv.get(),
+                                  envp.get(),
+                                  G_SPAWN_STDERR_TO_DEV_NULL,  // no flags
+                                  NULL,  // no setup function
+                                  NULL,  // no setup function data
+                                  &child_stdout,
+                                  NULL,  // stderr pointer
+                                  &exit_status,
+                                  &err);
 
-  static int call = 0;
-
-  if (call == 0) {
-      information->status_ = UPDATE_SUCCESSFUL;
-      information->version_ = NewStringCopy("Updated to 2.0");
-  }
-  if (call == 1) {
-    information->status_ = UPDATE_ALREADY_UP_TO_DATE;
-    information->version_ = NewStringCopy("Already running 1.0");
-  }
-  if (call == 2) {
+  if (!success || exit_status != 0) {
     information->status_ = UPDATE_ERROR;
-    information->version_ = NewStringCopy("Aw, Snap! 1.5");
+    if (!success) {
+      information->version_ = NewStringCopy(err->message);
+    } else {
+      information->version_ = NewStringCopy("Nonzero return code");
+    }
+    return false;
   }
 
-  ++call; call %= 3;
-
-  return information->status_ != UPDATE_ERROR;
+  if (child_stdout && !strcmp(child_stdout, "UPDATED")) {
+    information->status_ = UPDATE_SUCCESSFUL;
+    information->version_ = NewStringCopy("Updated to new version");
+    return true;
+  } else {
+    information->status_ = UPDATE_ERROR;
+    information->version_ = NewStringCopy("didn't update");
+    return false;
+  }
+  // TODO(adlr): handle up to date
+  //information->status_ = UPDATE_ALREADY_UP_TO_DATE;
+  //information->version_ = NewStringCopy("Didn't update");
 }
 
 extern "C"
 bool ChromeOSCheckForUpdate(UpdateInformation* information) {
-  // This code mochs in the UpdateInformation. To be filled in by Andrew.
+  scoped_array<char*> envp(new char*[2]);
+  envp[0] = strdup("PATH=/bin:/sbin:/usr/bin:/usr/sbin");
+  envp[1] = NULL;
+  
+  scoped_array<char*> argv(new char*[3]);
+  argv[0] = strdup("/opt/google/memento_updater/suid_exec");
+  argv[1] = strdup("/opt/google/memento_updater/ping_omaha.sh");
+  argv[2] = NULL;
 
-  // simulate time to check.
-  ::sleep(static_cast<unsigned>(std::rand()) % 30);
+  gchar* child_stdout = NULL;
+  gint exit_status = 0;
+  GError* err = NULL;
 
-  information->destruct_ = &Destruct;
+  gboolean success = g_spawn_sync(NULL,  // Use parent's working dir
+                                  argv.get(),
+                                  envp.get(),
+                                  G_SPAWN_STDERR_TO_DEV_NULL,  // flags
+                                  NULL,  // no setup function
+                                  NULL,  // no setup function data
+                                  &child_stdout,
+                                  NULL,  // stderr pointer
+                                  &exit_status,
+                                  &err);
 
-  static int call = 0;
-
-  if (call == 0) {
-      information->status_ = UPDATE_IS_AVAILABLE;
-      information->version_ = NewStringCopy("2.0 is available");
-  }
-  if (call == 1) {
-    information->status_ = UPDATE_ALREADY_UP_TO_DATE;
-    information->version_ = NewStringCopy("Already running 1.1");
-  }
-  if (call == 2) {
+  if (!success || exit_status != 0) {
     information->status_ = UPDATE_ERROR;
-    information->version_ = NewStringCopy("Aw, Snap! 1.6");
+    if (!success) {
+      information->version_ = NewStringCopy(err->message);
+    } else {
+      information->version_ = NewStringCopy("Nonzero return code");
+    }
+    return false;
   }
 
-  ++call; call %= 3;
+  if (strlen(child_stdout) > 0) {
+    string new_version = ValueForKey(child_stdout, "NEW_VERSION");
 
-  return information->status_ != UPDATE_ERROR;
+    information->status_ = UPDATE_IS_AVAILABLE;
+    information->version_ = NewStringCopy(new_version.c_str());
+  } else {
+    information->status_ = UPDATE_ALREADY_UP_TO_DATE;
+    information->version_ = NewStringCopy("No new version");
+  }
+  return true;
 }
 
 }  // namespace chromeos
