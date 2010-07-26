16c5e6d1e8fc3219cd996c7c886a6437a03ad5be
Yusuke Sato <yusukes@chromium.org>
2010-07-26 21:28:21 +0900

    Execute setxkbmap command only when it's necessary.
    Since the SetCurrentKeyboardLayoutByName() API is called every time when ibus-daemon sends FocusIn signal to Chrome, and the API call is relatevely expensive, it would be better to reduce the number of system() calls.
    
    - Created XKeyboard class and moved ChromeOSGetCurrentKeyboardLayoutName and ChromeOSSetCurrentKeyboardLayoutName implementations to the class.
    - Cache the current keyboard layout name in the class.
    
    BUG=none
    TEST=manually on EeePC
    
    Review URL: http://codereview.chromium.org/3052004

--- chromeos_keyboard.cc
+++ chromeos_keyboard.cc
@@ -9,6 +9,9 @@
 #include <base/string_util.h>
 #include <libxklavier/xklavier.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "base/singleton.h"
 
 namespace {
 
@@ -48,8 +51,102 @@ class XklEngineWrapper {
  private:
   Display* display_;
   XklEngine* xkl_engine_;
+
   DISALLOW_COPY_AND_ASSIGN(XklEngineWrapper);
 };
+
+// A singleton class which wraps the setxkbmap command.
+class XKeyboard {
+ public:
+  // Returns the singleton instance of the class. Use LeakySingletonTraits.
+  // We don't delete the instance at exit.
+  static XKeyboard* Get() {
+    return Singleton<XKeyboard, LeakySingletonTraits<XKeyboard> >::get();
+  }
+
+  bool SetLayout(const std::string& layout_name) {
+    // Use CapsLock as the second Windows-L key so netbook users could open
+    // and close NTP by pressing CapsLock.
+    std::string layouts_to_set = layout_name + "+capslock(super)";
+    if ((layouts_to_set.substr(0, 3) != "us+") &&
+        (layouts_to_set.substr(0, 3) != "us(")) {
+      layouts_to_set += ",us";
+    }
+
+    // Since executing setxkbmap takes more than 200 ms on EeePC, and this
+    // function is called on every focus-in event, try to reduce the number of
+    // the system() calls.
+    if (last_layout_ == layouts_to_set) {
+      return true;
+    }
+
+    std::string command = StringPrintf("setxkbmap -layout '%s'",
+                                       layouts_to_set.c_str());
+    const int status_code = system(command.c_str());
+    if (status_code == 0) {
+      XklEngineWrapper wrapper;
+      if (wrapper.Init() &&
+          !xkl_engine_is_group_per_toplevel_window(wrapper.xkl_engine())) {
+        // Use caching only when XKB setting is not per-window.
+        last_layout_ = layouts_to_set;
+      }
+      DLOG(INFO) << "XKB layout is changed to " << layouts_to_set;
+      return true;
+    }
+    LOG(INFO) << "Failed to change XKB layout to: " << layouts_to_set;
+    last_layout_.clear();  // invalidate the cache.
+    return false;
+  }
+
+  const std::string GetLayout() {
+    const char* cursor = NULL;
+    char buf[1024];  // Big enough for setxkbmap output.
+
+    if (!last_layout_.empty()) {
+      cursor = last_layout_.c_str();
+    } else {
+      FILE* input = popen("setxkbmap -print", "r");
+      if (!input) {
+        LOG(ERROR) << "popen() failed";
+        return "";
+      }
+      const size_t num_bytes = fread(buf, 1, sizeof(buf), input);
+      pclose(input);
+      if (num_bytes == 0) {
+        LOG(ERROR) << "fread() failed (nothings is read)";
+        return "";
+      }
+      // Parse a line like:
+      // xkb_symbols   { include "pc+us+capslock(super)+inet(pc105)"};
+      const char* cursor = strstr(buf, "pc+");
+      if (!cursor) {
+        LOG(ERROR) << "strstr() failed";
+        return "";
+      }
+      cursor += 3;  // Skip "pc+".
+    }
+
+    const char* next_plus = strstr(cursor, "+");
+    if (!next_plus) {
+      LOG(ERROR) << "strstr() failed";
+      return "";
+    }
+    const std::string layout_name = std::string(cursor, next_plus);
+    LOG(INFO) << "Current XKB layout name: " << layout_name;
+    return layout_name;
+  }
+
+ private:
+  friend struct DefaultSingletonTraits<XKeyboard>;
+  XKeyboard() {}
+  ~XKeyboard() {}
+
+  std::string last_layout_;  // ex "us+capslock(super)"
+
+  DISALLOW_COPY_AND_ASSIGN(XKeyboard);
+};
+
+}  // namespace
 
 extern "C"
 bool ChromeOSSetKeyboardLayoutPerWindow(bool is_per_window) {
@@ -83,59 +180,10 @@ bool ChromeOSGetKeyboardLayoutPerWindow(bool* is_per_window) {
 
 extern "C"
 bool ChromeOSSetCurrentKeyboardLayoutByName(const std::string& layout_name) {
-  // TODO(satorux): Rewrite this function with xklavier.
-  // The implementation is more like a prototype. Will rework later.
-
-  // Use CapsLock as the second Windows-L key so netbook users could open/close
-  // NTP by pressing CapsLock.
-  std::string layouts_to_set = layout_name + "+capslock(super)";
-  if ((layouts_to_set.substr(0, 3) != "us+") &&
-      (layouts_to_set.substr(0, 3) != "us(")) {
-    layouts_to_set += ",us";
-  }
-  std::string command = StringPrintf("setxkbmap -layout '%s'",
-                                     layouts_to_set.c_str());
-  const int status_code = system(command.c_str());
-  if (status_code == 0) {
-    return true;
-  }
-  LOG(INFO) << "Failed to change XKB layout to: " << layouts_to_set;
-  return false;
+  return XKeyboard::Get()->SetLayout(layout_name);
 }
 
 extern "C"
 const std::string ChromeOSGetCurrentKeyboardLayoutName() {
-  // TODO(satorux): Rewrite this function with xklavier.
-  // The implementation is more like a prototype. Will rework later.
-  FILE* input = popen("setxkbmap -print", "r");
-  if (!input) {
-    LOG(ERROR) << "popen() failed";
-    return "";
-  }
-  char buf[1024];  // Big enough for setxkbmap output.
-  const size_t num_bytes = fread(buf, 1, sizeof(buf), input);
-  pclose(input);
-  if (num_bytes == 0) {
-    LOG(ERROR) << "fread() failed (nothings is read)";
-    return "";
-  }
-
-  // Prase a line like:
-  // xkb_symbols   { include "pc+us+inet(evdev)"};
-  const char* cursor = strstr(buf, "pc+");
-  if (!cursor) {
-    LOG(ERROR) << "strstr() failed";
-    return "";
-  }
-  cursor += 3;  // Skip "pc+".
-  const char* next_plus = strstr(cursor, "+");
-  if (!next_plus) {
-    LOG(ERROR) << "strstr() failed";
-    return "";
-  }
-  const std::string layout_name = std::string(cursor, next_plus);
-  LOG(INFO) << "Current XKB layout name: " << layout_name;
-  return layout_name;
+  return XKeyboard::Get()->GetLayout();
 }
-
-}  // namespace chromeos
