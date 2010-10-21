4d3249e2176f87f10705696f7b644eece751c3d8
Dave Moore <davemoore@chromium.org>
2010-10-21 13:51:16 -0700

    Change system() calls to execl()
    Change-Id: I5ce499e1324a1121ae5a0153ee298d7216814783
    
    BUG=7971
    TEST=check chrome:system/login-times and confirm that we've improved.
    
    Review URL: http://codereview.chromium.org/3959002

--- chromeos_touchpad.cc
+++ chromeos_touchpad.cc
@@ -5,6 +5,7 @@
 #include "chromeos_touchpad.h"  // NOLINT
 
 #include <string>
+#include <sys/wait.h>
 
 #include "base/logging.h"
 #include "base/string_util.h"
@@ -18,17 +19,30 @@ const char* kTpControl = "/opt/google/touchpad/tpcontrol";
 
 extern "C"
 void ChromeOSSetTouchpadSensitivity(int value) {
-  std::string command = StringPrintf("%s sensitivity %d", kTpControl, value);
-  if (system(command.c_str()) < 0)
-    LOG(WARNING) << "Got error while running \"" << command << "\"";
+  pid_t child_pid = fork();
+  if (!child_pid) {
+    std::string value_string = StringPrintf("%d", value);
+    if (!daemon(1, 0)) {
+      execl(kTpControl, kTpControl, "sensitivity", value_string.c_str(), NULL);
+    }
+    _exit(0);
+  } else if (child_pid > 0) {
+    int status = 0;
+    waitpid(child_pid, &status, 0);
+  }
 }
 
 extern "C"
 void ChromeOSSetTouchpadTapToClick(bool enabled) {
-  std::string command =
-      StringPrintf("%s taptoclick %s", kTpControl, enabled ? "on" : "off");
-  if (system(command.c_str()) < 0)
-    LOG(WARNING) << "Got error while running \"" << command << "\"";
+  pid_t child_pid = fork();
+  if (!child_pid) {
+    if (!daemon(1, 0))
+      execl(kTpControl, kTpControl, "taptoclick", enabled ? "on" : "off", NULL);
+    _exit(0);
+  } else if (child_pid > 0) {
+    int status = 0;
+    waitpid(child_pid, &status, 0);
+  }
 }
 
 }  // namespace chromeos
